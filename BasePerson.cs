95b58cfb09d4ba4181896c4305e6e8c0fe4a0523
batuhan-uzun <batuhanuzun2356@gmail.com>
2024-09-05 07:01:40 +0300

    Add project files.

---
+++ BasePerson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProject
+{
+    public class BasePerson
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public void printName()
+        {
+            Console.WriteLine($"{Name} {Surname}");
+        }
+    }
+
+    public class Student : BasePerson
+    {
+        public int StudentID { get; set; }
+        public void printStudentInfo()
+        {
+            Console.WriteLine($"{Name} {Surname} {StudentID}");
+        }
+    }
+
+    public class Teacher : BasePerson
+    {
+        public int Salary { get; set; }
+        public void printTeacherInfo()
+        {
+            Console.WriteLine($"{Name} {Surname} {Salary}");
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using InheritanceProject;
+
+Teacher teacher = new Teacher()
+{
+    Name = "Ahmet",
+    Surname = "Yılmaz",
+    Salary = 30000
+};
+
+Student student = new Student()
+{
+    Name = "Mehmet",
+    Surname = "Yıldırım",
+    StudentID = 25
+};
+
+teacher.printTeacherInfo();
+student.printStudentInfo();
