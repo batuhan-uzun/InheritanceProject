a245f911f666e14e01ffdc632f6f9e767253cddb
Batuhan Uzun <batuhanuzun2356@gmail.com>
2024-09-05 07:05:44 +0300

    Main.cs

--- Main.cs
+++ Main.cs
@@ -1,18 +1,18 @@
 ﻿using InheritanceProject;
 
-Teacher teacher = new Teacher()
+Teacher teacher = new Teacher() // Created object from Teacher class
 {
     Name = "Ahmet",
     Surname = "Yılmaz",
     Salary = 30000
 };
 
-Student student = new Student()
+Student student = new Student() // Created object from Student class
 {
     Name = "Mehmet",
     Surname = "Yıldırım",
     StudentID = 25
 };
 
-teacher.printTeacherInfo();
-student.printStudentInfo();
+teacher.printTeacherInfo(); // Teacher information printed
+student.printStudentInfo(); // Student information printed
